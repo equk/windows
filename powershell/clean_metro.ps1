55daadc552f1ff800c6c43d4e1dd6fc697672ed1
equk <linux@equk.co.uk>
2015-08-01 17:09:41 +0100

    added require admin to header of ps1 & added some messages

--- powershell/clean_metro.ps1
+++ powershell/clean_metro.ps1
@@ -1,2 +1,7 @@
+#requires -version 4.0
+#requires -runasadministrator
+Write-Host "Removing All Metro/Market Apps"
 Get-AppXProvisionedPackage -online | Remove-AppxProvisionedPackage -online
 Get-AppxPackage -AllUsers | Remove-AppxPackage
+Clear-Host
+Write-Host "Metro/Market Apps Removed"

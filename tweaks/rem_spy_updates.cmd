5b06dbc5073b37d50d901edae72b3c07e4fee332
equk <linux@equk.co.uk>
2015-09-08 12:46:34 +0100

    added quiet and norestart to remove spy updates script windows

--- tweaks/rem_spy_updates.cmd
+++ tweaks/rem_spy_updates.cmd
@@ -1,21 +1,21 @@
 ECHO OFF
 ECHO Removing Dubious Windows Updates
 ECHO ================================
-wusa /uninstall /KB:3021917
-wusa /uninstall /KB:3050265
-wusa /uninstall /KB:3035583
-wusa /uninstall /KB:2952664
-wusa /uninstall /KB:2976978
-wusa /uninstall /KB:2990214
-wusa /uninstall /KB:3068708
-wusa /uninstall /KB:3022345
-wusa /uninstall /KB:2952664
-wusa /uninstall /KB:3075851
-wusa /uninstall /KB:3045999 
-wusa /uninstall /KB:2919355 
-wusa /uninstall /KB:3065987 
-wusa /uninstall /KB:3075851
-wusa /uninstall /KB:2977759
+wusa /uninstall /KB:3021917 /quiet /norestart
+wusa /uninstall /KB:3050265 /quiet /norestart
+wusa /uninstall /KB:3035583 /quiet /norestart
+wusa /uninstall /KB:2952664 /quiet /norestart
+wusa /uninstall /KB:2976978 /quiet /norestart
+wusa /uninstall /KB:2990214 /quiet /norestart
+wusa /uninstall /KB:3068708 /quiet /norestart
+wusa /uninstall /KB:3022345 /quiet /norestart
+wusa /uninstall /KB:2952664 /quiet /norestart
+wusa /uninstall /KB:3075851 /quiet /norestart
+wusa /uninstall /KB:3045999 /quiet /norestart
+wusa /uninstall /KB:2919355 /quiet /norestart
+wusa /uninstall /KB:3065987 /quiet /norestart
+wusa /uninstall /KB:3075851 /quiet /norestart
+wusa /uninstall /KB:2977759 /quiet /norestart
 ECHO All Updates Removed
 PAUSE
 EXIT

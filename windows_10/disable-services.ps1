00b144fa491c19b7a2232f8fb80fc8986f5733a2
equk <linux@equk.co.uk>
2015-09-11 14:05:24 +0100

    added new windows 10 folder & disable-services powershell script

---
+++ windows_10/disable-services.ps1
@@ -0,0 +1,17 @@
+<#
+.SYNOPSIS
+    Script to Disable Specific Services in Windows
+.DESCRIPTION
+    Script to Disable Specific Services in Windows
+    Made With Privacy In Mind But Can Be Adapted / Tweaked Accordingly
+    Requires Admin Access
+    Twitter: @equilibriumuk 
+.NOTES
+    File Name      : disable-services.ps1
+    Author         : @equilibriumuk
+    Prerequisite   : PowerShell V2
+    Copyright 2015 - B.Walden
+.LINK
+    Script posted on github:
+    https://github.com/equk
+#>

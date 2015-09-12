60930e760498fe932adfa52668aa18d8de6c5000
equk <linux@equk.co.uk>
2015-09-12 12:31:27 +0100

    created firewall rule script to block IP Addresses from powershell CLI

---
+++ windows_10/fw-block.ps1
@@ -0,0 +1,44 @@
+#requires -version 4.0
+#requires -runasadministrator
+<#
+.SYNOPSIS
+    Script to Block a IP Address In Windows Firewall
+.DESCRIPTION
+    Script to Block a IP Address In Windows Firewall
+    Made as a simple way to block IP addresses in powershell
+    Only really created as I use linux and like to use cli for admin tasks
+    Requires Admin Access
+    Twitter: @equilibriumuk 
+.PARAMETER Action
+    Valid Actions are Add or Remove
+.PARAMETER IpAddress
+    Input the IP address you would like to block in Windows Firewall
+    You can also remove a firewall rule based on this string
+.NOTES
+    File Name      : fw-block.ps1
+    Author         : @equilibriumuk
+    Prerequisite   : PowerShell V4
+    Copyright 2015 - B.Walden
+.LINK
+    Script posted on github:
+    https://github.com/equk
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true,Position=1)]
+    [ValidateSet("Add","Remove")]
+    [string] $Action,
+    [Parameter(Mandatory=$True)]
+    [string] $IpAddress)
+
+switch ($Action) {
+    "Add" {
+        Write-Verbose "Adding block to IP addresses in firewall"
+        New-NetFirewallRule -DisplayName "Block '$IpAddress'" -Group "CLI Added IP Block" -Action block -Direction out -Profile Any -Protocol Any -RemoteAddress $IpAddress
+    }
+    "Remove" {
+        Write-Verbose "Removing block to IP addresses in firewall"
+        Remove-NetFirewallRule -DisplayName "Block '$IpAddress'"
+    }
+}

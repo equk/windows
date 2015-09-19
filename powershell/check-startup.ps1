06b9b0bcf2cbf217b254b3cd442c7bbcf0a66a76
equk <linux@equk.co.uk>
2015-09-19 15:11:05 +0100

    added check-startup powershell script

---
+++ powershell/check-startup.ps1
@@ -0,0 +1,48 @@
+#requires -version 4.0
+#requires -runasadministrator
+
+<#
+.SYNOPSIS
+    Check Common Used Registry StartUp Locations
+.DESCRIPTION
+    Check Common Used Registry StartUp Locations
+    These Locations Are Often Used By Malware & Viruses But Are Also Used By
+    Software (eg: OneDrive)
+    Requires Admin Access
+    Twitter: @equilibriumuk 
+.NOTES
+    File Name      : misc-tweaks.ps1
+    Author         : @equilibriumuk
+    Prerequisite   : PowerShell V4
+.LINK
+    Script posted on github:
+    https://github.com/equk
+#>
+
+Write-Host ">> Check Common Used Registry StartUp Locations"
+
+$startupregs = @(
+    "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\Run"
+    "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run"
+    "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
+    "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunServices"
+    "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce"
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\Run"
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunServices"
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce"
+    "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows"
+    "HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Run"
+    "HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\Run"
+    "HKEY_USERS\.DEFAULT\Software\Microsoft\Windows NT\CurrentVersion\Windows\load"
+    "HKEY_USERS\.DEFAULT\Software\Microsoft\Windows NT\CurrentVersion\Windows\run"
+    )
+
+foreach ($startupreg in $startupregs) {
+    If ((Test-Path "$startupreg"))
+    {
+        Write-Host "++ Key $startupreg"
+        Get-ItemProperty $startupreg
+    }
+}

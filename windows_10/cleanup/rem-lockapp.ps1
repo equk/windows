4ebc70c57b3152a5c0ab828289219f68e6b3e805
equk <equk@users.noreply.github.com>
2016-09-04 15:31:38 +0100

    added powershell script to disable ugly lockscreen from anniversary update

---
+++ windows_10/cleanup/rem-lockapp.ps1
@@ -0,0 +1,33 @@
+﻿#requires -version 4.0
+#requires -runasadministrator
+
+<#
+.SYNOPSIS
+    Script to Disable Windows 10 LockApp
+.DESCRIPTION
+    Script to Disable Windows 10 LockApp
+    Disables LockScreen but unfortunately logon does not change as windows
+    has files in system32 & winSxS for persistence
+    Just one of many annoying changes from the anniversary update
+    Requires Admin Access
+    Twitter: @equilibriumuk
+.NOTES
+    File Name      : disable-lockapp.ps1
+    Author         : @equilibriumuk
+    Prerequisite   : PowerShell V4
+.LINK
+    Script posted on github:
+    https://github.com/equk
+#>
+
+$sysapppath = "$env:systemroot\SystemApps"
+$sysapps = @(
+    "Microsoft.LockApp_cw5n1h2txyewy"
+    )
+
+Write-Host ">> Moving Folders"
+foreach ($sysapp in $sysapps) {
+    if (Test-Path $sysapppath\$sysapp) {
+        mv $sysapppath\$sysapp $sysapppath\$sysapp"_disabled"
+    }
+}

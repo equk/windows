f30e53f6023a9741cda5b56db273cc593f6c711f
equk <equk@users.noreply.github.com>
2017-05-22 13:22:02 +0100

    updated enable_scripts.cmd to unblock powershell scripts in win10

--- windows_10/enable_scripts.cmd
+++ windows_10/enable_scripts.cmd
@@ -1,5 +1,13 @@
 @echo off
-echo allowing users to run powershell [.ps1] scripts
-powershell "Set-ExecutionPolicy Unrestricted -force 2>&1 | Out-Null"
-echo done.
+echo allowing users to run powershell scripts
+echo.
+echo for more information see: http://go.microsoft.com/fwlink/?LinkID-135170
+echo.
+pause
+echo setting execution policy
+powershell "Set-ExecutionPolicy Unrestricted"
+echo unblocking scripts included in this folder & subfolders
+powershell "ls -Recurse *.ps1 | Unblock-File"
+powershell "ls -Recurse *.psm1 | Unblock-File"
+echo all scripts within this folder should now run without security prompt
 pause

aa08f00672058e8680fbab7864d1c564bc3f4923
equk <equk@users.noreply.github.com>
2017-05-23 13:07:41 +0100

    added more registry keys to disable-cortana.ps1

--- windows_10/disable-cortana.ps1
+++ windows_10/disable-cortana.ps1
@@ -21,18 +21,33 @@
 # Disable Cortana
 Write-Host ">>>> Disabling Cortana"
 If (-Not (Test-Path "HKCU:\Software\Microsoft\Personalization\Settings")) {
-    Write-Host ">> Personalization Settings not found creating ..."
-    New-Item -Path "HKCU:\Software\Microsoft\Personalization\Settings"
+    Write-Host ">> Personalization Settings not found, creating ..."
+    New-Item -Path "HKCU:\Software\Microsoft\Personalization\Settings" -Force
 }
 sp "HKCU:\Software\Microsoft\Personalization\Settings" "AcceptedPrivacyPolicy" 0
+sp "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" "CortanaEnabled" 0
+If (-Not (Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search")) {
+    Write-Host ">> Windows Search not found, creating ..."
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
+    Write-Host ">>"
+}
+sp "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" "AllowCortana" 0
+Write-Host ">> Disabling Web Search ( bing ) ..."
+sp "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" "DisableWebSearch" 1
+Write-Host ">> Disabling Connected Search ..."
+sp "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" "ConnectedSearchPrivacy" 3
+sp "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" "ConnectedSearchSafeSearch" 3
+sp "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" "ConnectedSearchUseWeb" 0
+sp "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" "ConnectedSearchUseWebOverMeteredConnections" 0
 If (-Not (Test-Path "HKCU:\Software\Microsoft\InputPersonalization")) {
-    Write-Host ">> InputPersonalization not found creating ..."
-    New-Item -Path "HKCU:\Software\Microsoft\InputPersonalization"
+    Write-Host ">> InputPersonalization not found, creating ..."
+    New-Item -Path "HKCU:\Software\Microsoft\InputPersonalization" -Force
+    Write-Host ">>"
 }
 sp "HKCU:\Software\Microsoft\InputPersonalization" "RestrictImplicitTextCollection" 1
 sp "HKCU:\Software\Microsoft\InputPersonalization" "RestrictImplicitInkCollection" 1
 If (-Not (Test-Path "HKCU:\Software\Microsoft\InputPersonalization\TrainedDataStore")) {
-    Write-Host ">> Personalization TrainedDataStore not found creating ..."
+    Write-Host ">> Personalization TrainedDataStore not found, creating ..."
     New-Item -Path "HKCU:\Software\Microsoft\InputPersonalization\TrainedDataStore"
 }
 sp "HKCU:\Software\Microsoft\InputPersonalization\TrainedDataStore" "HarvestContacts" 0

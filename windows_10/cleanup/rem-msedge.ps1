e7b714b3c741da4814595888c9373be36247ba2e
equk <equk@users.noreply.github.com>
2018-05-05 18:37:47 +0100

    added script to remove Microsoft Edge (runs in background on 1803)

--- windows_10/cleanup/rem-msedge.ps1
+++ windows_10/cleanup/rem-msedge.ps1
@@ -3,17 +3,13 @@
 
 <#
 .SYNOPSIS
-    Script to Disable Pre-Installed SystemApps in Windows 10
+    Script to Disable Microsoft Edge in Windows 10
 .DESCRIPTION
-    Script to Disable Pre-Installed SystemApps in Windows 10
-    Cortana SearchUI.exe accesses the internet even with cortana disabled
-    I remove the other sysapps as they are reporting/feedback apps
-    Note: Make sure Cortana is first in list if req'd as process respawns quickly
-    Also if you get access denied, run the script twice
+    Script to Disable Microsoft Edge in Windows 10
     Requires Admin Access
     Twitter: @equilibriumuk
 .NOTES
-    File Name      : disable-services.ps1
+    File Name      : rem-msedge.ps1
     Author         : @equilibriumuk
     Prerequisite   : PowerShell V4
 .LINK
@@ -23,18 +19,12 @@
 
 $sysapppath = "$env:systemroot\SystemApps"
 $sysapps = @(
-    "Microsoft.Windows.Cortana_cw5n1h2txyewy"
-    "ContactSupport_cw5n1h2txyewy"
-    "ParentalControls_cw5n1h2txyewy"
-    "WindowsFeedback_cw5n1h2txyewy"
     "Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
     )
 
 Write-Host ">> Killing MS Edge Process"
 Get-Process *MicrosoftEdge* | Stop-Process -Force
 Get-Process *MicrosoftEdgeCP* | Stop-Process -Force
-Write-Host ">> Killing Cortana Process"
-Get-Process *SearchUI* | Stop-Process -Force
 Write-Host ">> Moving Folders"
 foreach ($sysapp in $sysapps) {
     [int]$i = "1"

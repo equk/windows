012c52cd256ae06c004ba91b01fddba7db620c37
equk <equk@users.noreply.github.com>
2018-05-13 11:23:06 +0100

    added script to remove unwanted optional features from windows 10

---
+++ windows_10/disable-optional.ps1
@@ -0,0 +1,37 @@
+#requires -version 4.0
+#requires -runasadministrator
+
+<#
+.SYNOPSIS
+    Script to Disable Optional Features in Windows 10
+.DESCRIPTION
+    Script to Disable Optional Features in Windows 10
+    Requires Admin Access
+    I have left "Printing-PrintToPDFServices-Features"
+    Twitter: @equilibriumuk
+.NOTES
+    File Name      : disable-services.ps1
+    Author         : @equilibriumuk
+    Prerequisite   : PowerShell V4
+.LINK
+    Script posted on github:
+    https://github.com/equk
+#>
+
+$features = @(
+    "MediaPlayback"
+    "SMB1Protocol"
+    "Xps-Foundation-Xps-Viewer"
+    "WorkFolders-Client"
+    "WCF-Services45"
+    "NetFx4-AdvSrvs"
+    "Printing-Foundation-Features"
+    "Printing-XPSServices-Features"
+    "MSRDC-Infrastructure"
+    "MicrosoftWindowsPowerShellV2Root"
+    "Internet-Explorer-Optional-amd64"
+)
+foreach ($feature in $features) {
+    Write-Host " Disabling Optional Feature $service ..."
+    Disable-WindowsOptionalFeature -Online -FeatureName $feature -NoRestart
+}

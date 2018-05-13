e9d8878d22048b41ef61cdf899f47538a0331bf7
equk <equk@users.noreply.github.com>
2018-05-13 11:19:26 +0100

    added a script for removing cortana searchui.exe (alternative to rem-sysapps)

---
+++ windows_10/cleanup/rem-cortana.ps1
@@ -0,0 +1,52 @@
+#requires -version 4.0
+#requires -runasadministrator
+
+<#
+.SYNOPSIS
+    Script to Remove Cortana in Windows 10
+.DESCRIPTION
+    Script to Remove Cortana in Windows 10
+    Cortana SearchUI.exe accesses the internet even with cortana disabled
+    If you get access denied, run the script twice
+    Requires Admin Access
+    Twitter: @equilibriumuk
+.NOTES
+    File Name      : rem-sysapps.ps1
+    Author         : @equilibriumuk
+    Prerequisite   : PowerShell V4
+.LINK
+    Script posted on github:
+    https://github.com/equk
+#>
+
+$sysapppath = "$env:systemroot\SystemApps"
+$sysapps = @(
+    "Microsoft.Windows.Cortana_cw5n1h2txyewy"
+    )
+
+Write-Host ">> Killing Cortana Process"
+Get-Process *SearchUI* | Stop-Process -Force
+Get-Process *RemindersServer* | Stop-Process -Force
+Get-Process *RuntimeBroker* | Stop-Process -Force
+Write-Host ">> Moving Folders"
+foreach ($sysapp in $sysapps) {
+    [int]$i = "1"
+    $dis = "_disabled"
+    $moveto = "$sysapppath\$sysapp$dis"
+    $movefrom = "$sysapppath\$sysapp"
+    if (Test-Path $sysapppath\$sysapp) {
+        if (Test-Path $moveto) {
+            do {
+                Write-Host ">> WARN: folder already exists"
+                Write-Host ">> moving app $sysapp to $moveto$i"
+                mv $sysapppath\$sysapp $moveto$i -EA SilentlyContinue
+                $i++
+                }
+            until (!(Test-Path $sysapppath\$sysapp))
+        }
+        else {
+            mv $sysapppath\$sysapp $moveto
+            Write-Host ">> moving app $sysapp to $moveto"
+        }
+    }
+}
